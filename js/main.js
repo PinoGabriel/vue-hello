4bd5ed97c860459363baa56f813e332b8977edf8
PinoGabriel <pinogabriel01@gmail.com>
2023-11-15 16:13:53 +0100

    feat: added img and classColor

--- js/main.js
+++ js/main.js
@@ -4,7 +4,14 @@
   createApp({
     data() {
       return {
-        message: 'Prima esercitazione di Vue'
+        message: 'Prima esercitazione di Vue',
+        image: `https://unsplash.it/600/300?image=171`,
+        classColor: "red"
       }
+    },
+    methods: {
+        btnClick: function(){
+            this.classColor = "blue"
+        }
     }
   }).mount('#app')
