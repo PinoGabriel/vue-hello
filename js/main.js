e5c9581dcce1114955b44642551288f783f6cb49
PinoGabriel <pinogabriel01@gmail.com>
2023-11-15 16:20:23 +0100

    feat: add prompt

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
   createApp({
     data() {
       return {
-        message: 'Prima esercitazione di Vue',
+        name: prompt("Scrivi il tuo nome"),
+        surname: prompt("Scrivi il tuo cognome"),
         image: `https://unsplash.it/600/300?image=171`,
         classColor: "red"
       }
